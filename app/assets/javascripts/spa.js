26dafb1c635e8721d33bb882fd1876bcb83d905b
Ehbraheem <ehbraheem@gmail.com>
2017-02-13 15:25:20 -0800

    Added all the required details for SPA

---
+++ app/assets/javascripts/spa.js
@@ -0,0 +1,10 @@
+/**
+ * Created by prof.BOLA on 2/13/2017.
+ * SPA JS manifest file
+ */
+
+//= require jquery
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource

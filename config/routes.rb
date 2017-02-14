0054b15150a3f2efa258794ec222233ac16787ee
Ehbraheem <ehbraheem@gmail.com>
2017-02-13 21:46:13 -0800

    re-added ui as root URL and merged AP+external

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
 
   # to solve AngularJS problem when served from the public directory
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
-  get '/', to: redirect('/client')
+  # get '/', to: redirect('/client')
 
-  # root 'ui#index'
-  # get '/ui' => 'ui#index'
-  # get '/ui#' => 'ui#index'
+  root 'ui#index'
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
